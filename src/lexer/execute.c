71e4fce79bc2ce4146bc12740071842739e1ff62
Adrian Jung <adrian.mael.jung@gmail.com>
2022-02-17 22:17:24 -0500

    norme

--- src/lexer/execute.c
+++ src/lexer/execute.c
@@ -1,33 +1,57 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   lexer.c                                            :+:      :+:    :+:   */
+/*   execute.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nsierra- <nsierra-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 01:34:11 by nsierra-          #+#    #+#             */
-/*   Updated: 2022/02/11 02:45:44 by nsierra-         ###   ########.fr       */
+/*   Updated: 2022/02/17 22:16:33 by nsierra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <limits.h>
-#include "lexer/tokens.h"
-
-#include <stddef.h>
 #include <stdio.h>
-#include <unistd.h>
-#include "ftprintf.h"
+#include "lexer/tokens.h"
 #include "lexer/lexer.h"
+#include "lexer/errors.h"
 
-static void	lexer_init(t_lexer *lexer, char *line)
+static int	classify_as_operator(t_token *token)
 {
-	ft_bzero(lexer, sizeof(t_lexer));
-	source_init(&lexer->src, line);
+	if (!ft_strncmp(token->raw, AND_IF, UINT_MAX))
+		token->type = TOK_AND_IF;
+	else if (!ft_strncmp(token->raw, OR_IF, UINT_MAX))
+		token->type = TOK_OR_IF;
+	else if (!ft_strncmp(token->raw, DLESS, UINT_MAX))
+		token->type = TOK_DLESS;
+	else if (!ft_strncmp(token->raw, DGREAT, UINT_MAX))
+		token->type = TOK_DGREAT;
+	else
+		return (0);
+	return (1);
 }
 
-static void	lexer_destroy(t_lexer *lexer)
+static int	classifier_single_operator_err(t_token *token)
 {
-	lst_destroy_nodes(&lexer->tokens, token_vdestroy);
+	return (!ft_strncmp(token->raw, "&", UINT_MAX));
+}
+
+static void	lexer_spot_operators(t_lexer *lexer)
+{
+	t_iter	iter;
+	t_token	*token;
+
+	if (lexer->status != LEXER_STATUS_DEFAULT)
+		return ;
+	iter_init(&iter, &lexer->tokens, ASC);
+	while (lexer->status == LEXER_STATUS_DEFAULT && iter_next(&iter))
+	{
+		token = (t_token *)iter.data;
+		if (classifier_single_operator_err(token))
+			lexer_syntax_error(lexer, token, LEXER_UNEXPECTED_TOK_ERROR);
+		else if (!classify_as_operator(token))
+			token->type = TOK_TOKEN;
+	}
 }
 
 static void	lexer_load_tokens(t_lexer *lexer)
@@ -52,69 +76,12 @@ static void	lexer_load_tokens(t_lexer *lexer)
 	}
 }
 
-static void lexer_syntax_error(t_lexer *lexer, t_token *token, t_lexer_status e)
-{
-	lexer->status = e;
-	if (e == LEXER_UNEXPECTED_TOK_ERROR)
-	{
-		ftfprintf(
-			STDERR_FILENO,
-			"%s: syntax error near unexpected token `%s'\n",
-			"minishell",
-			token->raw);
-	}
-}
-
-static int	classifier_single_operator_err(t_token *token)
-{
-	return (!ft_strncmp(token->raw, "&", UINT_MAX));
-}
-
-static int	classify_as_operator(t_token *token)
-{
-	if (!ft_strncmp(token->raw, AND_IF, UINT_MAX))
-		token->type = TOK_AND_IF;
-	else if (!ft_strncmp(token->raw, OR_IF, UINT_MAX))
-		token->type = TOK_OR_IF;
-	else if (!ft_strncmp(token->raw, DLESS, UINT_MAX))
-		token->type = TOK_DLESS;
-	else if (!ft_strncmp(token->raw, DGREAT, UINT_MAX))
-		token->type = TOK_DGREAT;
-	else
-		return (0);
-	return (1);
-}
-
-static void	lexer_params_classify(t_lexer *lexer)
-{
-	t_iter	iter;
-	t_token	*token;
-
-	if (lexer->status != LEXER_STATUS_DEFAULT)
-		return ;
-	iter_init(&iter, &lexer->tokens, ASC);
-	while (lexer->status == LEXER_STATUS_DEFAULT && iter_next(&iter))
-	{
-		token = (t_token *)iter.data;
-		if (classifier_single_operator_err(token))
-			lexer_syntax_error(lexer, token, LEXER_UNEXPECTED_TOK_ERROR);
-		else if (classify_as_operator(token))
-		{
-			printf("TOKEN %s TYPE %d\n", token->raw, token->type);
-			continue ;
-		}
-		else
-			token->type = TOK_TOKEN;
-		printf("TOKEN %s TYPE %d\n", token->raw, token->type);
-	}
-}
-
 void	lexer_execute(char *line)
 {
 	t_lexer		lexer;
 
 	lexer_init(&lexer, line);
 	lexer_load_tokens(&lexer);
-	lexer_params_classify(&lexer);
+	lexer_spot_operators(&lexer);
 	lexer_destroy(&lexer);
 }
--- src/lexer/init_destroy.c
+++ src/lexer/init_destroy.c
@@ -1,32 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   init_destroy.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nsierra- <nsierra-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/08 21:59:31 by nsierra-          #+#    #+#             */
-/*   Updated: 2022/02/17 22:03:21 by nsierra-         ###   ########.fr       */
+/*   Created: 2022/02/17 22:06:29 by nsierra-          #+#    #+#             */
+/*   Updated: 2022/02/17 22:10:45 by nsierra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <readline/readline.h>
-#include "libft.h"
 #include "lexer/lexer.h"
 
-int	main(void)
+void	lexer_init(t_lexer *lexer, char *line)
 {
-	char		*line;
+	ft_bzero(lexer, sizeof(t_lexer));
+	source_init(&lexer->src, line);
+}
 
-	while (42)
-	{
-		line = readline("$ ");
-		if (line == NULL)
-			break ;
-		lexer_execute(line);
-		free(line);
-	}
-	return (0);
+void	lexer_destroy(t_lexer *lexer)
+{
+	lst_destroy_nodes(&lexer->tokens, token_vdestroy);
 }
