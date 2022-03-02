64c7d896aad6e17ccba13a211d973100881f6719
Rakjlou <rakjlou@gmail.com>
2022-03-02 02:08:16 -0500

    Removed useless inclusion

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: nsierra- <nsierra-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 22:10:59 by nsierra-          #+#    #+#             */
-/*   Updated: 2022/02/09 01:33:45 by nsierra-         ###   ########.fr       */
+/*   Updated: 2022/03/02 02:05:29 by nsierra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,5 @@
 
 # define PS1_DEFAULT "minishell$ "
 # define PS2_DEFAULT "> "
-
-# include "lexer.h"
 
 #endif
