ff13a83aea2bfdf146b702adc005cb094e46d392
Rakjlou <rakjlou@gmail.com>
2022-03-02 10:34:53 -0500

    wip leaks on heredoc ctrl d

--- src/parser/tree/destroy.c
+++ src/parser/tree/destroy.c
@@ -6,12 +6,22 @@
 /*   By: nsierra- <nsierra-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 17:28:26 by nsierra-          #+#    #+#             */
-/*   Updated: 2022/03/02 03:45:26 by nsierra-         ###   ########.fr       */
+/*   Updated: 2022/03/02 05:56:56 by nsierra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "parser/parser.h"
+
+static void	redir_destroy(void *raw)
+{
+	t_redirection	*redirection;
+
+	redirection = raw;
+	if (token_is(redirection->type, TOK_DLESS))
+		free(redirection->arg);
+	free(raw);
+}
 
 void	exec_tree_destroy(t_command *command)
 {
@@ -29,7 +39,7 @@ void	exec_tree_destroy(t_command *command)
 	}
 	else if (command->type == COMMAND_COMPOUND)
 	{
-		lst_destroy_nodes(&command->data.compound.redirections, free);
+		lst_destroy_nodes(&command->data.compound.redirections, redir_destroy);
 		exec_tree_destroy(command->data.compound.tree);
 	}
 	free(command);
