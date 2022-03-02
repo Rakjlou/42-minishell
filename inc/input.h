f8ddcba5846d0c6811caa9f2c390c312cf923bf4
Rakjlou <rakjlou@gmail.com>
2022-03-02 18:41:55 -0500

    Improved flow

--- inc/input.h
+++ inc/input.h
@@ -6,7 +6,7 @@
 /*   By: nsierra- <nsierra-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 22:10:59 by nsierra-          #+#    #+#             */
-/*   Updated: 2022/03/02 17:15:53 by nsierra-         ###   ########.fr       */
+/*   Updated: 2022/03/02 18:37:30 by nsierra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@
 
 char	*input_get_line(void);
 char	*input_get_line2(void);
-char	*input_get_line_prompt(char *prompt);
 void	input_clear(void);
 
 #endif
