aa97e4fe543e5ed89ddeedb83f2ff6e2be7144d3
Rakjlou <rakjlou@gmail.com>
2022-03-02 18:41:55 -0500

    Improved flow

--- src/input/get_line.c
+++ src/input/get_line.c
@@ -6,7 +6,7 @@
 /*   By: nsierra- <nsierra-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 21:59:31 by nsierra-          #+#    #+#             */
-/*   Updated: 2022/03/02 06:02:46 by nsierra-         ###   ########.fr       */
+/*   Updated: 2022/03/02 18:37:31 by nsierra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,22 @@ static char	*input_save_line_to_history(char *line)
 	return (line);
 }
 
-char	*input_get_line(void)
-{
-	return (input_get_line_prompt(PS1_DEFAULT));
-}
-
-char	*input_get_line2(void)
-{
-	return (readline(PS2_DEFAULT));
-}
-
-char	*input_get_line_prompt(char *prompt)
+static char	*input_get_line_prompt(char *prompt, int history_save)
 {
 	char	*line;
 
 	line = readline(prompt);
-	if (line == NULL || !*line)
+	if (!history_save || line == NULL || !*line)
 		return (line);
 	return (input_save_line_to_history(line));
+}
+
+char	*input_get_line(void)
+{
+	return (input_get_line_prompt(PS1_DEFAULT, 1));
+}
+
+char	*input_get_line2(void)
+{
+	return (input_get_line_prompt(PS2_DEFAULT, 0));
 }
