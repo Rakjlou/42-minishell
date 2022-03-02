fd2abfae0b12e1c8e71f1227f18760504cd47b40
Adrian Jung <adrian.mael.jung@gmail.com>
2022-03-02 04:06:12 -0500

    Fixed parenthesis support, fixed memleak

--- src/parser/command/compound/build.c
+++ src/parser/command/compound/build.c
@@ -6,7 +6,7 @@
 /*   By: nsierra- <nsierra-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 17:52:17 by nsierra-          #+#    #+#             */
-/*   Updated: 2022/02/28 21:45:35 by nsierra-         ###   ########.fr       */
+/*   Updated: 2022/03/02 04:00:46 by nsierra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,18 @@ static int	valid_start(t_command **command)
 	);
 }
 
+static void	command_compound_build_subtree(
+	t_parser *parser,
+	t_iter *iter,
+	t_command_compound *cdata)
+{
+	++parser->subshell;
+	if (!parser_next_token_noendl(parser, iter))
+		return ;
+	exec_tree_build_recursive(parser, iter, &cdata->tree);
+	--parser->subshell;
+}
+
 void	command_compound_build(
 	t_parser *parser,
 	t_iter *iter,
@@ -41,16 +53,14 @@ void	command_compound_build(
 	compound->type = COMMAND_COMPOUND;
 	cdata = &compound->data.compound;
 	cdata->tree = NULL;
-	++parser->subshell;
-	if (!parser_next_token_noendl(parser, iter))
-		return ;
-	exec_tree_build_recursive(parser, iter, &cdata->tree);
-	--parser->subshell;
-	if (!token_is(iter->data, TOK_C_PARENTHESIS))
-		return (parser_unexpected_token(parser, iter->data));
+	command_compound_build_subtree(parser, iter, cdata);
 	compound->before = *command;
 	*command = compound;
-	if (!parser_next_token(parser, iter))
+	if (parser->status != PARSER_STATUS_DEFAULT)
+		return ;
+	else if (!token_is(iter->data, TOK_C_PARENTHESIS))
+		return (parser_unexpected_token(parser, iter->data));
+	else if (!parser_next_token(parser, iter))
 		return ;
 	while (token_is_redirection_operator(iter->data))
 		command_redirection_build(parser, iter, &cdata->redirections);
--- src/parser/command/redirection/build.c
+++ src/parser/command/redirection/build.c
@@ -6,7 +6,7 @@
 /*   By: nsierra- <nsierra-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 17:52:17 by nsierra-          #+#    #+#             */
-/*   Updated: 2022/03/02 01:32:42 by nsierra-         ###   ########.fr       */
+/*   Updated: 2022/03/02 03:40:34 by nsierra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,22 @@
 
 void	command_redirection_build(t_parser *parser, t_iter *iter, t_lst *lst)
 {
-	t_token			*token;
+	t_token			*type;
+	t_token			*arg;
 	t_redirection	*redirection;
-	t_redirection	candidate;
 
-	candidate.type = iter->data;
+	type = iter->data;
 	if (!parser_next_token_noendl(parser, iter))
 		return ;
 	else if (!token_is(iter->data, TOK_TOKEN))
 		return (parser_unexpected_token(parser, iter->data));
-	token = iter->data;
-	token->type = TOK_WORD;
-	candidate.arg = token;
+	arg = iter->data;
+	arg->type = TOK_WORD;
 	redirection = ft_calloc(sizeof(t_redirection), 1);
 	if (redirection == NULL)
 		return (parser_internal_error(parser));
-	redirection = ft_memcpy(redirection, &candidate, sizeof(t_redirection));
+	redirection->type = type;
+	redirection->arg = arg;
 	if (!lst_push_back(lst, redirection))
 		parser_internal_error(parser);
 	parser_next_token(parser, iter);
