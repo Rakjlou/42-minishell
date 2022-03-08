6c94b36599c7fe5c06ccd54b01a2d477fb2075cb
Rakjlou <rakjlou@gmail.com>
2022-03-08 21:31:53 +0100

    wip

--- src/parser/command/redirection/run.c
+++ src/parser/command/redirection/run.c
@@ -6,13 +6,51 @@
 /*   By: nsierra- <nsierra-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 17:52:17 by nsierra-          #+#    #+#             */
-/*   Updated: 2022/03/08 18:49:20 by nsierra-         ###   ########.fr       */
+/*   Updated: 2022/03/08 21:31:29 by nsierra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ftprintf.h"
 #include "wordexp.h"
 #include "parser/parser.h"
+
+#include <unistd.h>
+
+int		open_file(char *filename, int flags, int *fd)
+{
+	int	candidate;
+
+	candidate = open(filename, flags);
+
+}
+
+void	redirection_handle(t_command *command, t_redirection *redirection)
+{
+	char	*filename;
+
+	filename = redirection->arg->expanded[0];
+	if (token_is(redirection->type, TOK_SGREAT))
+		redirection->fd = open(filename, O_CREAT);
+}
+
+static int	is_ambiguous(t_redirection *redirection)
+{
+	return (
+		ft_cmatrix_size(redirection->arg->expanded) > 1
+		|| ft_cmatrix_size(redirection->arg->expanded) == 0
+		|| ft_strlen(redirection->arg->expanded[0]) == 0
+	);
+}
+
+static void	ambiguous_error(t_command *command, t_redirection *redirection)
+{
+	command->status = EXIT_FAILURE;
+	ftfprintf(
+		STDERR_FILENO,
+		"%s: %s: ambiguous redirect\n",
+		"minishell",
+		redirection->arg->raw);
+}
 
 void	redirections_run(t_command *command, t_lst *redirections)
 {
@@ -23,17 +61,13 @@ void	redirections_run(t_command *command, t_lst *redirections)
 	while (iter_next(&iter))
 	{
 		redirection = iter.data;
-		redirection->arg->expanded = wordexp(redirection->arg->raw);
-		if (ft_cmatrix_size(redirection->arg->expanded) > 1
-			|| ft_cmatrix_size(redirection->arg->expanded) == 0)
+		if (!token_is(redirection->type, TOK_DLESS))
 		{
-			command->status = 1;
-			ftfprintf(
-				STDERR_FILENO,
-				"%s: %s: ambiguous redirect\n",
-				"minishell",
-				redirection->arg->raw);
+			redirection->arg->expanded = wordexp(redirection->arg->raw);
+			if (is_ambiguous(redirection))
+				ambiguous_error(command, redirection);
 		}
+		redirection_handle(command, redirection);
 	}
 	return ;
 }
