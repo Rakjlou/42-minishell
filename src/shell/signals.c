6d6ab29ca73e10f728be3e713d31ee48fe266321
Rakjlou <rakjlou@gmail.com>
2022-03-08 19:02:59 +0100

    bit cleaning

--- src/shell/signals.c
+++ src/shell/signals.c
@@ -6,7 +6,7 @@
 /*   By: nsierra- <nsierra-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 21:59:31 by nsierra-          #+#    #+#             */
-/*   Updated: 2022/03/02 20:38:24 by nsierra-         ###   ########.fr       */
+/*   Updated: 2022/03/08 18:55:20 by nsierra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,19 @@
 #include "input.h"
 #include "shell.h"
 
-// static void	sigint_handler(int signal)
-// {
-// 	(void)signal;
-// 	ft_putstr_fd("\n", STDIN_FILENO);
-// 	rl_on_new_line();
-// 	rl_replace_line("", 0);
-// 	rl_redisplay();
-// }
+static void	sigint_handler(int signal)
+{
+	(void)signal;
+	ft_putstr_fd("\n", STDIN_FILENO);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
 
-// int	signals_init(void)
-// {
-// 	if (signal(SIGQUIT, SIG_IGN) == SIG_ERR
-// 		|| signal(SIGINT, sigint_handler) == SIG_ERR)
-// 		return (EXIT_FAILURE);
-// 	return (EXIT_SUCCESS);
-// }
+int	signals_init(void)
+{
+	if (signal(SIGQUIT, SIG_IGN) == SIG_ERR
+		|| signal(SIGINT, sigint_handler) == SIG_ERR)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
