723ade68cb9df0a63d90b4724477091896ad9164
Adrian Jung <adrian.mael.jung@gmail.com>
2022-03-10 21:49:31 +0100

    Correct handling of NULL values in envp loading

--- src/env/init.c
+++ src/env/init.c
@@ -6,11 +6,12 @@
 /*   By: nsierra- <nsierra-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 18:26:56 by ajung             #+#    #+#             */
-/*   Updated: 2022/03/02 21:02:15 by nsierra-         ###   ########.fr       */
+/*   Updated: 2022/03/10 21:49:04 by nsierra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
+#include "ftprintf.h"
 
 static char	*get_key(char *str)
 {
@@ -29,6 +30,8 @@ static char	*get_value(char *str)
 	i = 0;
 	while (str[i] != '=' && str[i])
 		i++;
+	if (str[i] == '=' && str[i + 1] == 0)
+		return (NULL);
 	return (ft_substr(str, i + 1, -1));
 }
 
@@ -39,7 +42,7 @@ static int	env_load_envp_variable(char *str)
 
 	value = get_value(str);
 	key = get_key(str);
-	if (!key || !value || env_set_value(key, value) == EXIT_FAILURE)
+	if (!key || env_set_value(key, value) == EXIT_FAILURE)
 		return (free(key), free(value), EXIT_FAILURE);
 	return (free(key), free(value), EXIT_SUCCESS);
 }
