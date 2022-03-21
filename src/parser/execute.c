1455c2b23655d7d984cecead1f93131def8e3e9c
Noe SIERRA VELASQUEZ <nsierra-@student.42.fr>
2022-03-21 21:50:08 +0100

    wip on pipelines

--- src/parser/execute.c
+++ src/parser/execute.c
@@ -6,11 +6,12 @@
 /*   By: nsierra- <nsierra-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 22:42:02 by nsierra-          #+#    #+#             */
-/*   Updated: 2022/03/09 21:07:31 by nsierra-         ###   ########.fr       */
+/*   Updated: 2022/03/21 20:20:31 by nsierra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "parser/parser.h"
+#include "shell.h"
+#include "ftprintf.h"
 
 void	parser_execute(void)
 {
@@ -21,7 +22,7 @@ void	parser_execute(void)
 	if (parser->status == PARSER_STATUS_FINISHED)
 	{
 		exec_tree_print(parser->tree, 0);
-		exec_tree_dispatch(parser->tree);
+		exec_tree_run(parser->tree);
 	}
 	parser_destroy();
 }
