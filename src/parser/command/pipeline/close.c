a275bee0d33fc9b0ad479ddcb4a86a86de886b03
Rakjlou <rakjlou@gmail.com>
2022-03-23 20:54:02 +0100

    ugly handling of pipeline builtins

--- src/parser/command/pipeline/close.c
+++ src/parser/command/pipeline/close.c
@@ -6,7 +6,7 @@
 /*   By: nsierra- <nsierra-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 17:52:17 by nsierra-          #+#    #+#             */
-/*   Updated: 2022/03/23 19:49:26 by nsierra-         ###   ########.fr       */
+/*   Updated: 2022/03/23 20:49:39 by nsierra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,20 @@
 #include "ftprintf.h"
 #include "shell.h"
 
+static int	process_exit_status(int status)
+{
+	if (WIFSIGNALED(status))
+		return (128 + WTERMSIG(status));
+	return (WEXITSTATUS(status));
+}
+
 void	pipeline_close(void)
 {
-	while (waitpid(-1, NULL, 0) != -1)
+	int	status;
+
+	status = EXIT_SUCCESS;
+	while (waitpid(-1, &status, 0) != -1)
 		;
+	if (_shell()->last_command)
+		_shell()->last_command->status = process_exit_status(status);
 }
