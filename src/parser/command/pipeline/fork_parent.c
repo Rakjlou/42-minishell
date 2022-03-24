ede9fc329073398576b47f2e45c41bcc0ce4b3d2
Noe SIERRA VELASQUEZ <nsierra-@student.42.fr>
2022-03-24 16:38:34 +0100

    Printing exit on ctrl+D

--- src/parser/command/pipeline/fork_parent.c
+++ src/parser/command/pipeline/fork_parent.c
@@ -6,28 +6,32 @@
 /*   By: nsierra- <nsierra-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 17:52:17 by nsierra-          #+#    #+#             */
-/*   Updated: 2022/03/24 14:18:10 by nsierra-         ###   ########.fr       */
+/*   Updated: 2022/03/24 15:55:28 by nsierra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include "ftprintf.h"
 #include "shell.h"
 
 void	pipeline_fork_parent(t_command *command)
 {
 	if (pipeline_is_first())
 	{
+		ftfprintf(STDERR_FILENO, "PARENT pipeline_is_first\n");
 		close(command->parent->data.pipeline.fds[PIPE_WRITE]);
 		_shell()->pipeline.l--;
 	}
 	else if (pipeline_is_last())
 	{
+		ftfprintf(STDERR_FILENO, "PARENT pipeline_is_last\n");
 		close(command->parent->data.pipeline.fds[PIPE_READ]);
 		_shell()->pipeline.r--;
 		pipeline_close();
 	}
 	else if (pipeline_is_middle())
 	{
+		ftfprintf(STDERR_FILENO, "PARENT pipeline_is_middle\n");
 		close(command->parent->parent->data.pipeline.fds[PIPE_READ]);
 		close(command->parent->data.pipeline.fds[PIPE_WRITE]);
 		_shell()->pipeline.l--;
