3c2a8517c7a0db36a2dbd28b177c461bdb41a3c6
Noe SIERRA VELASQUEZ <nsierra-@student.42.fr>
2022-03-24 15:20:45 +0100

    Added wordexp test

--- test.sh
+++ test.sh
@@ -15,14 +15,18 @@ test_file()
 	bash --posix < $file > $file.ref
 	local ref_exit_value=$?
 
-	./minishell < $file | grep -v minishell > $file.ours
+	./minishell < $file | grep -v "minishell\\$" > $file.ours
 	local our_exit_value=$?
 
-	diff $file.ref $file.ours > $file.diff
+	diff -y $file.ref $file.ours > $file.diff
 
 	if [ $? -ne 0 ]; then
 		echo "DIFF FAILED" $file;
 		cat $file.diff;
+		echo "===" "OURS" "==="
+		cat $file.ours;
+		echo "===" "REF" "==="
+		cat $file.ref;
 		test_file_exit $file 1;
 	elif [ $ref_exit_value -ne $our_exit_value ]; then
 		echo "EXIT VALUE FAILED" $file
@@ -40,4 +44,5 @@ test_file "tests/pipe_double" 2>/dev/null
 test_file "tests/pipe_triple" 2>/dev/null
 test_file "tests/logical_simple" 2>/dev/null
 test_file "tests/logical_mix" 2>/dev/null
+test_file "tests/wordexp" 2>/dev/null
 
