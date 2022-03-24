84dc56f460ddf69150b63f89e32ae1748c5d5c26
Rakjlou <rakjlou@gmail.com>
2022-03-24 15:35:33 +0100

    Fixed empty commands

--- src/parser/command/simple/exec/run.c
+++ src/parser/command/simple/exec/run.c
@@ -6,17 +6,20 @@
 /*   By: nsierra- <nsierra-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 17:52:17 by nsierra-          #+#    #+#             */
-/*   Updated: 2022/03/22 20:16:37 by nsierra-         ###   ########.fr       */
+/*   Updated: 2022/03/24 15:24:48 by nsierra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser/parser.h"
 
-static int	is_builtin(char *str)
+static int	is_builtin(t_command *command)
 {
+	char	*candidate;
 	char	*builtins[7];
 	int		i;
 
+	if (ft_cmatrix_size(command->argv) == 0)
+		return (0);
 	builtins[0] = "echo";
 	builtins[1] = "cd";
 	builtins[2] = "pwd";
@@ -24,10 +27,11 @@ static int	is_builtin(char *str)
 	builtins[4] = "unset";
 	builtins[5] = "env";
 	builtins[6] = "exit";
+	candidate = command->argv[0];
 	i = 0;
 	while (i < 7)
 	{
-		if (!ft_strncmp(builtins[i], str, -1))
+		if (!ft_strncmp(builtins[i], candidate, -1))
 			return (1);
 		++i;
 	}
@@ -36,11 +40,9 @@ static int	is_builtin(char *str)
 
 void	command_simple_run(t_command *command)
 {
-	if (command->status != 0
-		|| !command_build_argv(command)
-		|| ft_cmatrix_size(command->argv) == 0)
+	if (command->status != 0 || !command_build_argv(command))
 		return ;
-	else if (is_builtin(command->argv[0]))
+	else if (is_builtin(command))
 		builtin_exec(command);
 	else
 		command_exec(command);
