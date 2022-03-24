773a73107656e71d1c409fd592744f78de8faee3
Noe SIERRA VELASQUEZ <nsierra-@student.42.fr>
2022-03-24 15:03:14 +0100

    Added tests

--- test.sh
+++ test.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+test_file_exit()
+{
+	rm -rf $1.ref;
+	rm -rf $1.diff;
+	rm -rf $1.ours;
+	return $2;
+}
+
 test_file()
 {
 	local file=$1
@@ -7,24 +15,29 @@ test_file()
 	bash --posix < $file > $file.ref
 	local ref_exit_value=$?
 
-	./minishell < $file | sed --expression '1d;$d' > $file.ours
+	./minishell < $file | grep -v minishell > $file.ours
 	local our_exit_value=$?
 
 	diff $file.ref $file.ours > $file.diff
 
 	if [ $? -ne 0 ]; then
 		echo "DIFF FAILED" $file;
-		cat file.diff;
-		return 1;
+		cat $file.diff;
+		test_file_exit $file 1;
 	elif [ $ref_exit_value -ne $our_exit_value ]; then
 		echo "EXIT VALUE FAILED" $file
 		echo "Ref" $ref_exit_value
 		echo "Ours" $our_exit_value
-		return 1;
+		test_file_exit $file 1;
 	else
 		echo "OK" $file;
-		return 0;
+		test_file_exit $file 0;
 	fi
 }
 
-test_file "tests/pipe_simple"
+test_file "tests/pipe_simple" 2>/dev/null
+test_file "tests/pipe_double" 2>/dev/null
+test_file "tests/pipe_triple" 2>/dev/null
+test_file "tests/logical_simple" 2>/dev/null
+test_file "tests/logical_mix" 2>/dev/null
+
