f2f767ec34d46783e2b7ec1a2a1e36104063ceb8
Adrian Jung <adrian.mael.jung@gmail.com>
2022-03-24 19:32:28 +0100

    handling signal

--- src/shell/signals/signal_child.c
+++ src/shell/signals/signal_child.c
@@ -1,38 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   init.c                                             :+:      :+:    :+:   */
+/*   signal_child.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ajung <ajung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/08 21:59:31 by nsierra-          #+#    #+#             */
-/*   Updated: 2022/03/24 18:28:48 by ajung            ###   ########.fr       */
+/*   Created: 2022/03/24 18:10:27 by ajung             #+#    #+#             */
+/*   Updated: 2022/03/24 18:33:30 by ajung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <unistd.h>
-#include "shell.h"
 #include "handle_signal.h"
 
-static void	param_init(int argc, char **argv)
+int	handle_signal_child(void)
 {
-	extern char	**environ;
-	t_shell		*shell;
-
-	shell = _shell();
-	shell->param.argc = argc;
-	shell->param.argv = argv;
-	shell->param.env = environ;
-	shell->pipeline.pipe_in = STDIN_FILENO;
-	shell->pipeline.pipe_out = STDOUT_FILENO;
-}
-
-int	shell_init(int argc, char **argv)
-{
-	param_init(argc, argv);
-	if ((env_init() != EXIT_SUCCESS)
-		|| handle_signals(MAIN_PROCESS) != EXIT_SUCCESS)
+	if (signal(SIGINT, SIG_DFL) == SIG_ERR
+		|| signal(SIGQUIT, SIG_DFL) == SIG_ERR)
 		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
