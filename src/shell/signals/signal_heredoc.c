43f90c6f44e7459a7c8a399e6612c9122a981c02
Adrian Jung <adrian.mael.jung@gmail.com>
2022-03-24 19:55:39 +0100

    norme

--- src/shell/signals/signal_heredoc.c
+++ src/shell/signals/signal_heredoc.c
@@ -3,21 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   signal_heredoc.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ajung <ajung@student.42.fr>                +#+  +:+       +#+        */
+/*   By: nsierra- <nsierra-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 18:05:55 by ajung             #+#    #+#             */
-/*   Updated: 2022/03/24 19:18:29 by ajung            ###   ########.fr       */
+/*   Updated: 2022/03/24 19:54:19 by nsierra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "handle_signal.h"
 
-void	sigint_handler_heredoc(int mescouilles)
+void	sigint_handler_heredoc(int signum)
 {
 	t_shell	*shell;
-	
+
+	(void)signum;
 	shell = _shell();
-	(void) mescouilles;
 	shell->last_command_status = 130;
 	ftprintf("\n");
 	return ;
@@ -25,7 +25,6 @@ void	sigint_handler_heredoc(int mescouilles)
 
 int	handle_signal_heredoc(void)
 {
-	//readline function ?
 	if (signal(SIGINT, sigint_handler_heredoc) == SIG_ERR
 		|| signal(SIGQUIT, SIG_IGN) == SIG_ERR)
 		return (EXIT_FAILURE);
