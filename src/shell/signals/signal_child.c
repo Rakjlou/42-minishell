bb009971996898ae81bb7a43a2513b756a251b28
Adrian JUNG <ajung@e2r5p2.clusters.42paris.fr>
2022-03-25 20:07:44 +0100

    work on Ctrl c dans cat mais pas vraiment

--- src/shell/signals/signal_child.c
+++ src/shell/signals/signal_child.c
@@ -6,11 +6,20 @@
 /*   By: ajung <ajung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 18:10:27 by ajung             #+#    #+#             */
-/*   Updated: 2022/03/24 18:33:30 by ajung            ###   ########.fr       */
+/*   Updated: 2022/03/25 19:57:12 by ajung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "handle_signal.h"
+
+// void	sigint_handler_child(int signum)
+// {
+// 	(void) signum;
+// 	ft_putstr_fd("\n", STDIN_FILENO);
+// 	rl_on_new_line();
+// 	rl_replace_line("", 0);
+// 	rl_redisplay();
+// }
 
 int	handle_signal_child(void)
 {
