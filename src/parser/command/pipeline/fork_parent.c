b0d9d58e7dfd04473af9f413ee4f31363841d273
Rakjlou <rakjlou@gmail.com>
2022-03-28 22:26:15 -0400

    Fixed a lot of routes.

--- src/parser/command/pipeline/fork_parent.c
+++ src/parser/command/pipeline/fork_parent.c
@@ -6,7 +6,7 @@
 /*   By: nsierra- <nsierra-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 17:52:17 by nsierra-          #+#    #+#             */
-/*   Updated: 2022/03/24 17:39:14 by nsierra-         ###   ########.fr       */
+/*   Updated: 2022/03/28 22:23:47 by nsierra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,50 @@
 #include "ftprintf.h"
 #include "shell.h"
 
+static void	handle_first(t_command *command)
+{
+	t_command	*parent;
+
+	parent = pipeline_get_parent(command);
+	if (parent == NULL)
+		return ;
+	close(parent->data.pipeline.fds[PIPE_WRITE]);
+	_shell()->pipeline.l--;
+}
+
+static void	handle_middle(t_command *command)
+{
+	t_command	*parent;
+	t_command	*previous;
+
+	parent = pipeline_get_parent(command);
+	previous = pipeline_get_previous(command);
+	if (parent == NULL || previous == NULL)
+		return ;
+	close(previous->data.pipeline.fds[PIPE_READ]);
+	close(parent->data.pipeline.fds[PIPE_WRITE]);
+	_shell()->pipeline.l--;
+	_shell()->pipeline.r--;
+}
+
+static void	handle_last(t_command *command)
+{
+	t_command	*parent;
+
+	parent = pipeline_get_parent(command);
+	if (parent == NULL)
+		return ;
+	close(parent->data.pipeline.fds[PIPE_READ]);
+	_shell()->pipeline.r--;
+	pipeline_close();
+}
+
 void	pipeline_fork_parent(t_command *command)
 {
 	if (pipeline_is_first())
-	{
-		close(pipeline_get_parent(command)->data.pipeline.fds[PIPE_WRITE]);
-		_shell()->pipeline.l--;
-	}
+		handle_first(command);
 	else if (pipeline_is_last())
-	{
-		close(pipeline_get_parent(command)->data.pipeline.fds[PIPE_READ]);
-		_shell()->pipeline.r--;
-		pipeline_close();
-	}
+		handle_last(command);
 	else if (pipeline_is_middle())
-	{
-		close(pipeline_get_previous(command)->data.pipeline.fds[PIPE_READ]);
-		close(pipeline_get_parent(command)->data.pipeline.fds[PIPE_WRITE]);
-		_shell()->pipeline.l--;
-		_shell()->pipeline.r--;
-	}
+		handle_middle(command);
 }
