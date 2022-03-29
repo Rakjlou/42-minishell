551ce1e4049f67cdcfab6160b5861733d805aebb
Adrian Jung <adrian.mael.jung@gmail.com>
2022-03-28 22:10:16 -0400

    Somehow lost 2 routes

--- libft/libft/ft_cmatrix_join.c
+++ libft/libft/ft_cmatrix_join.c
@@ -6,19 +6,44 @@
 /*   By: nsierra- <nsierra-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 15:48:53 by nsierra-          #+#    #+#             */
-/*   Updated: 2022/03/09 00:52:31 by nsierra-         ###   ########.fr       */
+/*   Updated: 2022/03/28 22:07:34 by nsierra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
+static char	**hydrate_result(char **result, char **matrix1, char **matrix2)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (matrix1 && matrix1[i])
+	{
+		result[j] = ft_strdup(matrix1[i]);
+		if (result[j] == NULL)
+			return (ft_cmatrix_free(result), NULL);
+		++j;
+		++i;
+	}
+	i = 0;
+	while (matrix2 && matrix2[i])
+	{
+		result[j] = ft_strdup(matrix2[i]);
+		if (result[j] == NULL)
+			return (ft_cmatrix_free(result), NULL);
+		++j;
+		++i;
+	}
+	return (result);
+}
+
 char	**ft_cmatrix_join(char **matrix1, char **matrix2)
 {
 	char	**result;
 	size_t	total;
-	int		i;
-	int		j;
 
 	total = 0;
 	if (matrix1)
@@ -28,12 +53,5 @@ char	**ft_cmatrix_join(char **matrix1, char **matrix2)
 	result = ft_calloc(sizeof(char *), total + 1);
 	if (result == NULL)
 		return (NULL);
-	i = 0;
-	j = 0;
-	while (matrix1 && matrix1[i])
-		result[j++] = ft_strdup(matrix1[i++]);
-	i = 0;
-	while (matrix2 && matrix2[i])
-		result[j++] = ft_strdup(matrix2[i++]);
-	return (result);
+	return (hydrate_result(result, matrix1, matrix2));
 }
