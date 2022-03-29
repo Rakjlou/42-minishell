3915b86592f524036940869b71e082eb7201acb5
Rakjlou <rakjlou@gmail.com>
2022-03-28 20:15:49 -0400

    fixed leak 1

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: nsierra- <nsierra-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 21:59:31 by nsierra-          #+#    #+#             */
-/*   Updated: 2022/03/10 18:18:35 by nsierra-         ###   ########.fr       */
+/*   Updated: 2022/03/28 20:11:04 by nsierra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 int	main(int argc, char **argv)
 {
 	if (shell_init(argc, argv) != EXIT_SUCCESS)
-		return (perror("minishell"), EXIT_FAILURE);
+		return (perror("minishell"), shell_destroy(), EXIT_FAILURE);
 	shell_loop();
 	shell_destroy();
 	return (_last_command_status());
